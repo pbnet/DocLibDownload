2a9e9963b5c753d28a38ea07961b322e8bf90188
Administrator@pbnet.pbnet.local <Administrator@pbnet.pbnet.local>
2020-07-26 04:42:48 +0300

    Add project files.

---
+++ DoclibDownload/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Net;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WssDownload4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 3)
+            {
+                Console.BackgroundColor = ConsoleColor.DarkBlue;
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Doclib Download for SharePoint");
+                Console.ResetColor();
+                Console.WriteLine("Usage: doclibdownload <site url> <library name> <download path>");
+                Console.WriteLine("Example: doclibdownload http://sharepoint.pbnet.pbnet.local SharedDocs c:\\downloads");
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.ResetColor();
+                return; 
+            }
+            try
+            {
+                SPDownload sp = new SPDownload(); 
+                sp.Execute(args[0],args[1],args[2]);
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Operation completed.");
+                Console.ResetColor();
+                Console.ReadKey(); 
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message + Environment.NewLine + ex.StackTrace); 
+            }
+
+        }
+    }
+}
---
+++ DoclibDownload/SPDownload.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.SharePoint;
+using System.IO; 
+
+
+namespace WssDownload4
+{
+    public class SPDownload
+    {
+        public string _DownloadPath = ""; 
+        public void Execute(string url,string listName, string downloadpath)
+        {
+            
+            if (!Directory.Exists(downloadpath))
+            {
+            needkey:
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("Folder does not exist. Do you want to create it (Y/N) ?" );
+                Console.ResetColor();
+                var key = Console.ReadKey();
+                if (key.Key == ConsoleKey.N)
+                {
+                    return;
+                }
+                else if (key.Key == ConsoleKey.Y)
+                {
+                    Directory.CreateDirectory(downloadpath);
+                    
+                }
+                else
+                    goto needkey;
+                
+            }
+            _DownloadPath = downloadpath;
+            Console.WriteLine("Connecting To Site: " + url); 
+            using (SPSite site = new SPSite(url))
+            {
+                using (SPWeb web = site.OpenWeb())
+                {
+                    SPList list = web.Lists[listName];
+                    StartRecursiv(list.RootFolder, _DownloadPath);
+                }
+            }
+
+        }
+
+        protected void StartRecursiv(SPFolder folder, string FolderPath)
+        {
+            download(folder, FolderPath);
+            foreach (SPFolder myfolder in folder.SubFolders)
+            {
+                recursiv(myfolder, FolderPath);
+            } 
+        }
+
+        protected  void recursiv(SPFolder folder,string FolderPath) 
+        {
+            FolderPath += "\\" + folder.Name ;
+            if (!Directory.Exists(FolderPath))
+            {
+                Directory.CreateDirectory(FolderPath); 
+            }
+            download(folder,FolderPath); 
+
+            foreach (SPFolder myfolder in folder.SubFolders) 
+            { 
+                recursiv(myfolder,FolderPath); 
+            } 
+        }
+
+        protected  void download(SPFolder folder,string FolderPath)
+        {
+            foreach (SPFile file in folder.Files)
+            {
+                Console.Write("Downloading ->" + FolderPath.Substring(_DownloadPath.Length) + "\\" + file.Name + ":");
+                try
+                {
+                    byte[] filebytes = file.OpenBinary();
+                    FileStream fs = new FileStream(FolderPath + "\\" + file.Name, FileMode.Create);
+                    fs.Write(filebytes, 0, filebytes.Length);
+                    fs.Close();
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.Write("OK " + "FileSize: " + filebytes.Length + " byte" );
+                    Console.WriteLine("");
+                    Console.ResetColor();
+                }
+                catch (Exception ex)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.Write("Failed");
+                    Console.WriteLine(ex.Message);
+                    Console.ResetColor();
+                }
+            }
+        }
+    }
+}
+
+
